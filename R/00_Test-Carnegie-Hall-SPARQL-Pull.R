a2bd11b53c4372540b6d17e05366fd501289ac6c
Nate <nathancday@gmail.com>
2019-02-15 07:39:28 -0500

    instrument-performer query and data as RDS

--- R/00_Test-Carnegie-Hall-SPARQL-Pull.R
+++ R/00_Test-Carnegie-Hall-SPARQL-Pull.R
@@ -4,9 +4,10 @@
 
 # 00 Load Packages --------------------------------------------------------
 
-require(tidyverse)
-require(jsonlite)
 require(RCurl)
+require(jsonlite)
+require(tidyverse)
+
 
 # 01 Use Sample Query HTML  -----------------------------------------------
 
@@ -24,3 +25,35 @@ ch_test_df <- ch_test %>%
   select(results) %>% 
   unnest()
 
+ch_test_df
+
+
+# 02 Instrument-Performer Query ----------------------------------------------
+
+# # Raw SPARQL
+# PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
+# PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+# PREFIX gndo: <http://d-nb.info/standards/elementset/gnd#>
+# PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+# PREFIX schema: <http://schema.org/>
+#   
+#   select ?name ?birthPlace ?birthDate ?label where {
+#     ?performer foaf:name ?name ;
+#     dbpedia-owl:birthPlace ?birthPlace ;
+#     schema:birthDate ?birthDate ;
+#     gndo:playedInstrument ?instrument .
+#     ?instrument rdfs:label ?label .
+#   }
+
+instrument <- 
+  getURL("http://data.carnegiehall.org/sparql/select?query=%23Find%20alto%20saxophonists%20born%20in%20November%0A%0A%23%20templated%20%5E%5E%5E%5E%0A%20PREFIX%20dbpedia-owl%3A%20%3Chttp%3A%2F%2Fdbpedia.org%2Fontology%2F%3E%0A%20PREFIX%20foaf%3A%20%3Chttp%3A%2F%2Fxmlns.com%2Ffoaf%2F0.1%2F%3E%0A%20PREFIX%20gndo%3A%20%3Chttp%3A%2F%2Fd-nb.info%2Fstandards%2Felementset%2Fgnd%23%3E%0A%20PREFIX%20rdfs%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F2000%2F01%2Frdf-schema%23%3E%0A%20PREFIX%20schema%3A%20%3Chttp%3A%2F%2Fschema.org%2F%3E%0A%20%0A%20select%20%3Fname%20%3FbirthPlace%20%3FbirthDate%20%3Flabel%20where%20%7B%0A%20%20%20%20%20%3Fperformer%20foaf%3Aname%20%3Fname%20%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20dbpedia-owl%3AbirthPlace%20%3FbirthPlace%20%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20schema%3AbirthDate%20%3FbirthDate%20%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20gndo%3AplayedInstrument%20%3Finstrument%20.%0A%20%20%20%20%20%3Finstrument%20rdfs%3Alabel%20%3Flabel%20.%0A%20%7D&out=json&key=") %>% 
+  fromJSON(flatten = TRUE) %>% 
+  as_tibble() %>% 
+  select(results) %>% 
+  unnest() %>%
+  select(matches("value"))
+
+dim(instrument)
+head(instrument)
+
+saveRDS(instrument, "data/intrument_performer.RDS")
--- README.md
+++ README.md
@@ -3,9 +3,17 @@ Repo for Test Carnegie Hall Shiny App
 
 **Original Carnegie Hall Repo** [https://github.com/CarnegieHall/linked-data](https://github.com/CarnegieHall/linked-data)
 
+[Carnegie's Query Builder](http://data.carnegiehall.org/sparql/#?query=SELECT%20*%20WHERE%20%7B%0A%20%20%3Fsubject%20%3Fproperty%20%3Fobject%0A%7D%20LIMIT%20100&writer=browse)
+
 *Sample Wikidata Queries from [Rob Hudson](https://github.com/robertmhudson)* 
 Here is a map that shows birthplaces of pianists that have performed at CH, with layers on the map filtered by population of the city:
 http://tinyurl.com/yb9qw775
  
 Here is a similar map, showing pianists that have performed at CH with layers filtered by sign of the zodiac:
 http://tinyurl.com/y7cjxzmj
+
+** Resources for Learning SPARQL **
+
+* [Apache Jena Tutorial](https://jena.apache.org/tutorials/sparql_basic_patterns.html)
+* [Presentation: Joins in SPARQL](http://www.cs.utexas.edu/~cannata/cs345/New%20Class%20Notes/15%20JoinsinSPARQL%20(3).pdf) by Andrew Oldag
+
